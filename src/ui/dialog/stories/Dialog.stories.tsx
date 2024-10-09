0f244dedf7c819296c50422a5a9f11124bdba266
sjmitch-git <sjmitch20@outlook.com>
2024-10-09 10:18:07 +0100

    Adds Dialog docs

--- src/ui/dialog/stories/Dialog.stories.tsx
+++ src/ui/dialog/stories/Dialog.stories.tsx
@@ -1,14 +1,71 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { Dialog } from '..'
 import { DialogProps } from '../types'
-import { Tabs, Progress } from '@/ui'
+import { Tabs } from '@/ui'
 import { LoginRegister } from '../../tabs/stories/Tabs.stories'
-import { Downloading } from '../../progress/stories/Progress.stories'
+
+const codeExample = `<Dialog
+  open={isOpen}
+  modal={false}
+  title="Dialog Title"
+  titleSize="lg"
+  titleBold={true}
+  onClose={() => console.log('Dialog closed')}
+>
+  <p>A simple dialog message!</p>
+</Dialog>`
+
+const modalExample = `<Dialog
+  open={isOpen}
+  modal={true}
+  title="Log-in to your account"
+  titleSize="lg"
+  titleBold={true}
+  onClose={() => console.log('Dialog closed')}
+>
+  <Tabs {...props}>{children}</Tabs>
+</Dialog>`
 
 const meta: Meta = {
 	title: 'Fluid UI/Feedback/Dialog',
 	component: Dialog,
+	parameters: {
+		docs: {
+			source: {
+				code: codeExample,
+			},
+			description: {
+				component: `
+The **Dialog** component displays a modal or non-modal dialog box with customizable content. It can be used to create a prompt or notification that the user needs to respond to.
+
+### Import
+\`\`\`tsx
+import { Dialog } from '@smitch/fluid'
+\`\`\`
+
+### Example Usage:
+\`\`\`tsx
+// simple dialog
+${codeExample}
+\`\`\`
+
+\`\`\`tsx
+// modal dialog
+${modalExample}
+\`\`\`
+
+### Props:
+- \`open\` (boolean): Whether the dialog is open or closed.
+- \`modal\` (boolean, optional): Whether the dialog is modal (blocks background interaction). Defaults to true.
+- \`title\` (string, optional): The title of the dialog.
+- \`titleSize\` ('sm' | 'md' | 'lg' | 'xl', optional): The size of the dialog title. Defaults to 'lg'.
+- \`titleBold\` (boolean, optional): Whether the dialog title is bold. Defaults to false.
+- \`onClose\` (function): A callback function that fires when the dialog is closed.
+        `,
+			},
+		},
+	},
 	tags: ['autodocs'],
 } satisfies Meta<typeof Dialog>
 
@@ -24,10 +81,10 @@ export const SimpleDialog: Story = (args: DialogProps) => {
 		if (args.onClose) args.onClose()
 	}
 
-	const handleSubmit = (data: { [key: string]: string }) => {
+	/* const handleSubmit = (data: { [key: string]: string }) => {
 		console.log('data', data)
 		handleClose()
-	}
+	} */
 
 	return (
 		<>
@@ -80,46 +137,58 @@ SimpleDialog.argTypes = {
 	},
 }
 
-export const ModalDialog: Story = (args: DialogProps) => {
-	const [open, setOpen] = useState(args.open)
+export const ModalDialog: Story = {
+	parameters: {
+		docs: {
+			source: {
+				code: `${modalExample}`,
+			},
+		},
+	},
+	render: (args) => {
+		const [open, setOpen] = useState(false)
 
-	const handleClose = () => {
-		setOpen(false)
-		if (args.onClose) args.onClose()
-	}
+		const handleClose = () => {
+			setOpen(false)
+			if (args.onClose) args.onClose()
+		}
 
-	const handleSubmit = (data: { [key: string]: string }) => {
-		console.log('data', data)
-		handleClose()
-	}
-
-	return (
-		<>
-			<button
-				onClick={() => setOpen(true)}
-				className='bg-blue-800 font-semibold text-white px-4 py-2 rounded hover:bg-blue-600'
-			>
-				Log-in
-			</button>
-			<Dialog
-				{...args}
-				open={open}
-				onClose={handleClose}
-			>
-				<Tabs {...LoginRegister.args}>{LoginRegister.args?.children}</Tabs>
-			</Dialog>
-		</>
-	)
+		return (
+			<>
+				<button
+					onClick={() => setOpen(true)}
+					className='bg-blue-800 font-semibold text-white px-4 py-2 rounded hover:bg-blue-600'
+				>
+					Log-in
+				</button>
+				<Dialog
+					{...args}
+					open={open}
+					onClose={handleClose}
+				>
+					<Tabs {...LoginRegister.args}>{LoginRegister.args?.children}</Tabs>
+				</Dialog>
+			</>
+		)
+	},
+	args: {
+		title: 'Log-in to your account',
+		titleSize: 'lg',
+		titleBold: true,
+		open: false,
+		modal: true,
+		onClose: () => console.log('Dialog closed'),
+	},
 }
 
-ModalDialog.args = {
+/* ModalDialog.args = {
 	title: 'Log-in to your account',
 	titleSize: 'lg',
 	titleBold: true,
 	open: false,
 	modal: true,
 	onClose: () => console.log('Dialog closed'),
-}
+} */
 
 ModalDialog.argTypes = {
 	...SimpleDialog.argTypes,
